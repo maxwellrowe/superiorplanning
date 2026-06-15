d4295454da5b6433e93986fd3477752cf2e506d4
Maxwell Rowe <max@mackeycreativelab.com>
2026-06-15 10:44:28 -0700

    udpate header

--- wp-content/themes/superior-planning/header.php
+++ wp-content/themes/superior-planning/header.php
@@ -7,23 +7,35 @@
 </head>
 <body <?php body_class(); ?>>
   <header id="header" class="bg-light d-flex justify-content-center align-items-center">
-    <div class="container">
+    <div class="container-xxl">
       <div class="row align-items-center">
-        <div class="col col-lg-3 d-none d-lg-block">
-          &nbsp;
-        </div>
-        <div class="col-6 col-lg-6 text-start text-lg-center">
+        <div class="col-4 col-lg-3 text-start">
           <a href="<?php echo home_url(); ?>" id="brand">
             <img src="<?php echo get_field('logo', 'option'); ?>" alt="" />
           </a>
         </div>
-        <div class="col-6 col-lg-3 text-end">
-          <?php if(get_field('account_access_page', 'option')) { ?>
-            <?php $account_page = get_field('account_access_page', 'option'); ?>
-            <a href="<?php echo get_permalink($account_page->ID); ?>" class="btn btn-outline-dark btn-sm"><?php echo get_the_title($account_page->ID); ?></a>
-            <?php } else { ?>
-              &nbsp;
-            <?php } ?>
+        <div class="col-8 col-lg-9 text-end">
+          <div class="d-flex align-items-center justify-content-end gap-3">
+            <?php
+            wp_nav_menu([
+              'theme_location'  => 'primary',
+              'container'       => 'nav',
+              'container_class' => 'primary-nav d-inline-block',
+              'menu_class'      => 'nav justify-content-end align-items-center gap-4 lh-1 text-center',
+              'fallback_cb'     => false,
+              'depth'           => 2,
+            ]);
+            ?>
+            <?php if ($header_cta = get_field('call_to_action_button', 'option')) : 
+              $header_cta_url    = esc_url($header_cta['url']);
+              $header_cta_title  = esc_html($header_cta['title'] ?: 'Learn More');
+              $header_cta_target = !empty($header_cta['target']) ? esc_attr($header_cta['target']) : '_self';
+            ?>
+              <a href="<?php echo $header_cta_url; ?>" target="<?php echo $header_cta_target; ?>" class="btn btn-outline-dark lh-1">
+                <?php echo $header_cta_title; ?>
+              </a>
+            <?php endif; ?>
+          </div>
         </div>
       </div>
     </div>
@@ -34,17 +46,17 @@
         <div class="row gap-0 row-cols-1 row-cols-md-2">
           <?php if(get_field('wealth_management_and_family_office_page', 'option')) { ?>
             <?php $wm_page = get_field('wealth_management_and_family_office_page', 'option'); ?>
-            <a class="col d-block header-subnav-wm" href="<?php echo home_url(); ?>/#wealth-management">
+            <a class="col d-block header-subnav-wm" href="<?php echo home_url(); ?>/#retirement-planning">
               <div class="d-flex align-items-center justify-content-center gap-2">
-                <span>Wealth Management &amp; Family Office</span><span class="fa-sharp fa-arrow-right"></span>
+                <span>Retirement Planning</span><span class="fa-sharp fa-arrow-right"></span>
               </div>
             </a>
           <?php } ?>
           <?php if(get_field('financial_solutions_group_page', 'option')) { ?>
             <?php $fs_page = get_field('financial_solutions_group_page', 'option'); ?>
-            <a class="col d-block header-subnav-fs" href="<?php echo home_url(); ?>/#financial-solutions">
+            <a class="col d-block header-subnav-fs" href="<?php echo home_url(); ?>/#real-estate-planning">
               <div class="d-flex align-items-center justify-content-center gap-2">
-                <span>Financial Solutions Group</span><span class="fa-sharp fa-arrow-right"></span>
+                <span>Real Estate Planning</span><span class="fa-sharp fa-arrow-right"></span>
               </div>
             </a>
           <?php } ?>
