d6155c15c38fa28b85b37f84499fd16d570090de
Maxwell Rowe <max@mackeycreativelab.com>
2026-06-15 11:18:44 -0700

    update page.php

--- wp-content/themes/superior-planning/page.php
+++ wp-content/themes/superior-planning/page.php
@@ -5,7 +5,23 @@
 				<h1 class="display-5 text-uppercase fw-bold text-center mb-4"><?php the_title(); ?></h1>
 				<?php
 					$page_id = get_queried_object_id();
-					content_builder($page_id);
+					$use_beaver_builder_content = false;
+
+					if ( class_exists( 'FLBuilderModel' ) ) {
+						if ( method_exists( 'FLBuilderModel', 'is_builder_enabled' ) ) {
+							$use_beaver_builder_content = FLBuilderModel::is_builder_enabled( $page_id );
+						}
+
+						if ( ! $use_beaver_builder_content && method_exists( 'FLBuilderModel', 'is_builder_active' ) ) {
+							$use_beaver_builder_content = FLBuilderModel::is_builder_active( $page_id );
+						}
+					}
+
+					if ( $use_beaver_builder_content ) {
+						the_content();
+					} else {
+						content_builder( $page_id );
+					}
 				?>
 			</div>
 		</div>
